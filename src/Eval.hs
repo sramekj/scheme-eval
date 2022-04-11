50a7c792b53d23ec9da3e05a92a6c31f7fd730b8
Jan Sramek <jan.sramek.ix@gmail.com>
2022-04-11 20:48:17 +0200

    add list ops

--- src/Eval.hs
+++ src/Eval.hs
@@ -46,11 +46,16 @@ primitives =
   , ("string?"  , strBoolBinOp (>))
   , ("string<=?", strBoolBinOp (<=))
   , ("string>=?", strBoolBinOp (>=))
+  , ("car"      , car)
+  , ("cdr"      , cdr)
+  , ("cons"     , cons)
+  , ("eq?"      , eqv)
+  , ("eqv?"     , eqv)
   ]
 
 numBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError LispVal
 numBinOp op singleVal@[_] = throwError $ NumArgs 2 singleVal
-numBinOp op params        = mapM unpackNum params <&> (Number . foldl1 op)
+numBinOp op params        = mapM unpackNum params <&> Number . foldl1 op
 
 unpackNum :: LispVal -> ThrowsError Integer
 unpackNum (Number n) = return n
@@ -89,3 +94,39 @@ numBoolBinOp = boolBinOp unpackNum
 strBoolBinOp = boolBinOp unpackStr
 
 boolBoolBinOp = boolBinOp unpackBool
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x : xs)        ] = return x
+car [DottedList (x : xs) _] = return x
+car [badArg               ] = throwError $ TypeMismatch "pair" badArg
+car badArgList              = throwError $ NumArgs 1 badArgList
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x : xs)        ] = return $ List xs
+cdr [DottedList [xs    ] x] = return x
+cdr [DottedList (_ : xs) x] = return $ DottedList xs x
+cdr [badArg               ] = throwError $ TypeMismatch "pair" badArg
+cdr badArgList              = throwError $ NumArgs 1 badArgList
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List []            ] = return $ List [x1]
+cons [x , List xs            ] = return $ List $ [x] <> xs
+cons [x , DottedList xs xlast] = return $ DottedList ([x] <> xs) xlast
+cons [x1, x2                 ] = return $ DottedList [x1] x2
+cons badArgList                = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [Bool   arg1, Bool arg2  ] = return $ Bool $ arg1 == arg2
+eqv [Number arg1, Number arg2] = return $ Bool $ arg1 == arg2
+eqv [String arg1, String arg2] = return $ Bool $ arg1 == arg2
+eqv [Atom   arg1, Atom arg2  ] = return $ Bool $ arg1 == arg2
+eqv [DottedList xs x, DottedList ys y] =
+  eqv [List $ xs <> [x], List $ ys <> [y]]
+eqv [List arg1, List arg2] = return $ Bool $ length arg1 == length arg2 && and
+  (zipWith (curry eqvPair) arg1 arg2)
+ where
+  eqvPair (x1, x2) = case eqv [x1, x2] of
+    Right (Bool val) -> val
+    _                -> False
+eqv [_, _]     = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
