300114b177c3edcdebd4ba737772cd1dab04e688
Jan Sramek <jan.sramek.ix@gmail.com>
2022-04-11 22:19:54 +0200

    add var functions

--- src/Eval.hs
+++ src/Eval.hs
@@ -5,9 +5,12 @@ module Eval
   , IOThrowsError(..)
   ) where
 
+import           Control.Exception              ( evaluate )
 import           Control.Monad.Except
 import           Data.Functor                   ( (<&>) )
 import           Data.IORef
+import           Data.Maybe                     ( isJust )
+import           GHC.GHCi.Helpers               ( evalWrapper )
 import           Parsers
 
 type Env = IORef [(String, IORef LispVal)]
@@ -165,3 +168,40 @@ equal [arg1, arg2] = do
   eqvEquals <- eqv [arg1, arg2]
   return $ Bool (primitiveEquals || let (Bool x) = eqvEquals in x)
 equal badArgList = throwError $ NumArgs 2 badArgList
+
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef <&> (isJust . lookup var)
+
+getVar :: Env -> String -> IOThrowsError LispVal
+getVar envRef var = do
+  env <- liftIO $ readIORef envRef
+  maybe (throwError $ UnboundVar "Getting an unbound variable" var)
+        (liftIO . readIORef)
+        (lookup var env)
+
+setVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+setVar envRef var value = do
+  env <- liftIO $ readIORef envRef
+  maybe (throwError $ UnboundVar "Setting an unbound variable" var)
+        (liftIO . flip writeIORef value)
+        (lookup var env)
+  return value
+
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value = do
+  alreadyDefined <- liftIO $ isBound envRef var
+  if alreadyDefined
+    then setVar envRef var value >> return value
+    else liftIO $ do
+      valueRef <- newIORef value
+      env      <- readIORef envRef
+      writeIORef envRef ((var, valueRef) : env)
+      return value
+
+bindVars :: Env -> [(String, LispVal)] -> IO Env
+bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef
+ where
+  extendEnv bindings env = fmap (<> env) (mapM addBinding bindings)
+  addBinding (var, value) = do
+    ref <- newIORef value
+    return (var, ref)
