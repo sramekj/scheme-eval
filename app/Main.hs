518a9c3843bbfee027f79515003bf5dc4b99adfb
Jan Sramek <jan.sramek.ix@gmail.com>
2022-04-11 21:16:44 +0200

    add repl

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,43 @@
 module Main where
 
+import           Control.Applicative            ( liftA2 )
 import           Control.Monad                  ( liftM )
 import           Eval                           ( eval )
+import           GHC.Event.Windows              ( processRemoteCompletion )
 import           Parsers                        ( extractValue
                                                 , readExpr
                                                 , trapError
                                                 )
 import           System.Environment
+import           System.IO               hiding ( try )
+
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr =
+  return $ extractValue $ trapError (fmap show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result then return () else action result >> until_ pred prompt action
+
+runRepl :: IO ()
+runRepl = until_ (liftA2 (||) (== "quit") (== ":q"))
+                 (readPrompt "Lisp>>> ")
+                 evalAndPrint
 
 main :: IO ()
 main = do
   args <- getArgs
-  let evaled = fmap show $ readExpr (head args) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+  case length args of
+    0 -> runRepl
+    1 -> evalAndPrint $ head args
+    _ -> putStrLn "Program takes only 0 or 1 argument"
